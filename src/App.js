e64d234e2c93bd039680209a661df0d15794e39e
Alexander Smith <hongzeyuan0@gmail.com>
2024-06-13 17:14:07 +0800

    美团案例静态页面初始化

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 // import ReplyList from "./replyList"; //评论案例
-import ReactReduxDemo from "./react-redux";
+// import ReactReduxDemo from "./react-redux";
+import ShoppingCart from "./shopping-cart";
 
 function App() {
   return (
     <div className="App">
       {/* <ReplyList /> */}
-      <ReactReduxDemo />
+      {/* <ReactReduxDemo /> */}
+      <ShoppingCart />
     </div>
   );
 }
