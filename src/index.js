84b29635e62cc4c0c0121bb397263ae1c2f609cd
Alexander Smith <hongzeyuan0@gmail.com>
2024-06-23 19:38:01 +0800

    antd 主题色定制

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import store from "./store";
+import "./theme.scss";
 import { Provider } from "react-redux";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
