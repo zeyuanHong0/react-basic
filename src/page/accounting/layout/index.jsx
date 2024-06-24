c923d72191a8fb656bbbdf759b832741576eaeab
Alexander Smith <hongzeyuan0@gmail.com>
2024-06-24 20:55:53 +0800

    TabBar搭建

--- src/page/accounting/layout/index.jsx
+++ src/page/accounting/layout/index.jsx
@@ -1,7 +1,45 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import { fetchBillList } from "@/store/modules/billStore";
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
+import { Badge, TabBar } from "antd-mobile";
+import {
+  BillOutline,
+  AddCircleOutline,
+  CalculatorOutline,
+} from "antd-mobile-icons";
+import "./index.scss";
+
+const Bottom = () => {
+  const navigate = useNavigate();
+  const handleChangeTab = (key) => {
+    navigate(key);
+  };
+  const tabs = [
+    {
+      key: "/",
+      title: "月度",
+      icon: <BillOutline />,
+    },
+    {
+      key: "/new",
+      title: "新建",
+      icon: <AddCircleOutline />,
+    },
+    {
+      key: "/year",
+      title: "年度",
+      icon: <CalculatorOutline />,
+    },
+  ];
+  return (
+    <TabBar onChange={(key) => handleChangeTab(key)}>
+      {tabs.map((item) => (
+        <TabBar.Item key={item.key} icon={item.icon} title={item.title} />
+      ))}
+    </TabBar>
+  );
+};
 
 const Layout = () => {
   const dispatch = useDispatch();
@@ -9,9 +47,13 @@ const Layout = () => {
     dispatch(fetchBillList());
   }, [dispatch]);
   return (
-    <div>
-      我是 layout
-      <Outlet />
+    <div className="page">
+      <div className="content-wrap">
+        <Outlet />
+      </div>
+      <div className="bottom">
+        <Bottom />
+      </div>
     </div>
   );
 };
