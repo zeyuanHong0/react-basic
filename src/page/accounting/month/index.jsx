3522dadbf120153ec7a09fc5353d022281970124
Alexander Smith <hongzeyuan0@gmail.com>
2024-06-27 17:21:00 +0800

    优化计算统计值相关的方法

--- src/page/accounting/month/index.jsx
+++ src/page/accounting/month/index.jsx
@@ -7,71 +7,53 @@ import dayjs from "dayjs";
 import _ from "lodash";
 import DailyBill from "./components/dayBill";
 
-const useYearMonth = () => {
-  const [date, setDate] = useState("");
-  useEffect(() => {
-    const current = new Date();
-    setDate(dayjs(current).format("YYYY-MM"));
-  }, []);
-  return {
-    date,
-    setDate,
-  };
-};
-
 const Month = () => {
-  const { date, setDate } = useYearMonth();
-  const [showDatePicker, setShowDatePicker] = useState(false);
-  const [total, setTotal] = useState({
-    pay: 0,
-    income: 0,
-  });
-  const initTotal = () => {
-    setTotal({
-      pay: 0,
-      income: 0,
-    });
-  };
   const { billList } = useSelector((state) => state.bill);
   // 对 billList进行处理
   const billListByMonth = useMemo(() => {
     // 按月分组 =>{yyyy-mm:[]}
     return _.groupBy(billList, (item) => dayjs(item.date).format("YYYY-MM"));
   }, [billList]);
+  const [showDatePicker, setShowDatePicker] = useState(false);
+  const [currentDate, setCurrentDate] = useState(() =>
+    dayjs().format("YYYY-MM")
+  );
+  const [currentMonthList, setCurrentMonthList] = useState([]);
+
+  const overview = useMemo(() => {
+    console.log("重新计算了");
+    // 计算每个月的总支出和总收入
+    let pay = 0;
+    let income = 0;
+    if (currentMonthList && currentMonthList.length > 0) {
+      currentMonthList.map((item) => {
+        if (item.type === "pay") {
+          pay += item.money;
+        } else if (item.type === "income") {
+          income += item.money;
+        }
+      });
+    }
+    return { pay, income, balance: income + pay };
+  }, [currentMonthList]);
 
   const handleShowDatePicker = () => {
     setShowDatePicker(true);
   };
-
-  // 计算总额
-  const calculateTotal = (list) => {
-    if (!list) {
-      initTotal();
-      return;
-    }
-    let allPay = 0;
-    let allIncome = 0;
-    list.map((item) => {
-      if (item.type === "pay") {
-        allPay += item.money;
-      } else if (item.type === "income") {
-        allIncome += item.money;
-      }
-    });
-    setTotal({
-      pay: allPay,
-      income: allIncome,
-    });
-  };
   // 确认日期
   const handleConfirmDate = (val) => {
-    const selectedDate = new Date(val);
-    setDate(dayjs(selectedDate).format("YYYY-MM"));
+    const monthKey = dayjs(val).format("YYYY-MM");
+    setCurrentDate(monthKey);
+    setCurrentMonthList(billListByMonth[monthKey]);
   };
 
   useEffect(() => {
-    calculateTotal(billListByMonth[date]);
-  }, [billListByMonth, date]);
+    const list = billListByMonth[dayjs().format("YYYY-MM")];
+    console.log(list);
+    if (list && list.length > 0) {
+      setCurrentMonthList(list);
+    }
+  }, [billListByMonth]);
 
   return (
     <div className="monthlyBill">
@@ -82,7 +64,7 @@ const Month = () => {
         <div className="header">
           {/* 时间切换区域 */}
           <div className="date">
-            <span className="text">{date}月账单</span>
+            <span className="text">{currentDate}月账单</span>
             <span
               className={classNames("arrow", { expand: showDatePicker })}
               onClick={() => handleShowDatePicker()}
@@ -91,17 +73,15 @@ const Month = () => {
           {/* 统计区域 */}
           <div className="twoLineOverview">
             <div className="item">
-              <span className="money">{total.pay.toFixed(2)}</span>
+              <span className="money">{overview.pay.toFixed(2)}</span>
               <span className="type">支出</span>
             </div>
             <div className="item">
-              <span className="money">{total.income.toFixed(2)}</span>
+              <span className="money">{overview.income.toFixed(2)}</span>
               <span className="type">收入</span>
             </div>
             <div className="item">
-              <span className="money">
-                {(total.pay + total.income).toFixed(2)}
-              </span>
+              <span className="money">{overview.balance.toFixed(2)}</span>
               <span className="type">结余</span>
             </div>
           </div>
