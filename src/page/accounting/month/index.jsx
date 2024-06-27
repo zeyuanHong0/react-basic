2b4448400b26281cd191863653fa54b1b22accfb
Alexander Smith <hongzeyuan0@gmail.com>
2024-06-27 16:09:03 +0800

    根据选择的月份展示对应的统计数据

--- src/page/accounting/month/index.jsx
+++ src/page/accounting/month/index.jsx
@@ -21,21 +21,57 @@ const useYearMonth = () => {
 const Month = () => {
   const { date, setDate } = useYearMonth();
   const [showDatePicker, setShowDatePicker] = useState(false);
+  const [total, setTotal] = useState({
+    pay: 0,
+    income: 0,
+  });
+  const initTotal = () => {
+    setTotal({
+      pay: 0,
+      income: 0,
+    });
+  };
   const { billList } = useSelector((state) => state.bill);
   // 对 billList进行处理
   const billListByMonth = useMemo(() => {
     // 按月分组 =>{yyyy-mm:[]}
     return _.groupBy(billList, (item) => dayjs(item.date).format("YYYY-MM"));
   }, [billList]);
-  console.log(billListByMonth);
+
   const handleShowDatePicker = () => {
     setShowDatePicker(true);
   };
+
+  // 计算总额
+  const calculateTotal = (list) => {
+    if (!list) {
+      initTotal();
+      return;
+    }
+    let allPay = 0;
+    let allIncome = 0;
+    list.map((item) => {
+      if (item.type === "pay") {
+        allPay += item.money;
+      } else if (item.type === "income") {
+        allIncome += item.money;
+      }
+    });
+    setTotal({
+      pay: allPay,
+      income: allIncome,
+    });
+  };
+  // 确认日期
   const handleConfirmDate = (val) => {
     const selectedDate = new Date(val);
     setDate(dayjs(selectedDate).format("YYYY-MM"));
-    setShowDatePicker(false);
   };
+
+  useEffect(() => {
+    calculateTotal(billListByMonth[date]);
+  }, [billListByMonth, date]);
+
   return (
     <div className="monthlyBill">
       <NavBar className="nav" backArrow={false}>
@@ -54,15 +90,17 @@ const Month = () => {
           {/* 统计区域 */}
           <div className="twoLineOverview">
             <div className="item">
-              <span className="money">{100}</span>
+              <span className="money">{total.pay.toFixed(2)}</span>
               <span className="type">支出</span>
             </div>
             <div className="item">
-              <span className="money">{200}</span>
+              <span className="money">{total.income.toFixed(2)}</span>
               <span className="type">收入</span>
             </div>
             <div className="item">
-              <span className="money">{200}</span>
+              <span className="money">
+                {(total.pay + total.income).toFixed(2)}
+              </span>
               <span className="type">结余</span>
             </div>
           </div>
@@ -73,7 +111,7 @@ const Month = () => {
             precision="month"
             visible={showDatePicker}
             max={new Date()}
-            onCancel={() => setShowDatePicker(false)}
+            onClose={() => setShowDatePicker(false)}
             onConfirm={handleConfirmDate}
           />
         </div>
