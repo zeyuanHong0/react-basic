26b67ac50b382c0689f3e23448f8872834ba8aeb
Alexander Smith <hongzeyuan0@gmail.com>
2024-06-28 10:05:31 +0800

    单日列表条件渲染

--- src/page/accounting/month/components/dayBill/index.jsx
+++ src/page/accounting/month/components/dayBill/index.jsx
@@ -2,6 +2,7 @@ import classNames from "classnames";
 import "./index.scss";
 import { useMemo } from "react";
 import { billTypeToName } from "@/page/accounting/common";
+import { useState } from "react";
 
 const DailyBill = ({ bills, date }) => {
   const dayResult = useMemo(() => {
@@ -20,12 +21,18 @@ const DailyBill = ({ bills, date }) => {
     return { pay, income, balance: income + pay };
   }, [bills]);
 
+  // 是否展示单日列表
+  const [showDetail, setShowDetail] = useState(false);
+
   return (
     <div className={classNames("dailyBill")}>
       <div className="header">
         <div className="dateIcon">
           <span className="date">{date}</span>
-          <span className={classNames("arrow")}></span>
+          <span
+            className={classNames("arrow", { expand: showDetail })}
+            onClick={() => setShowDetail(!showDetail)}
+          ></span>
         </div>
         <div className="oneLineOverview">
           <div className="pay">
@@ -43,20 +50,22 @@ const DailyBill = ({ bills, date }) => {
         </div>
       </div>
       {/* 单日列表 */}
-      <div className="billList">
-        {bills.map((item) => {
-          return (
-            <div className="bill" key={item.id}>
-              <div className="detail">
-                <div className="billType">{billTypeToName[item.useFor]}</div>
+      {showDetail && (
+        <div className="billList">
+          {bills.map((item) => {
+            return (
+              <div className="bill" key={item.id}>
+                <div className="detail">
+                  <div className="billType">{billTypeToName[item.useFor]}</div>
+                </div>
+                <div className={classNames("money", item.type)}>
+                  {item.money.toFixed(2)}
+                </div>
               </div>
-              <div className={classNames("money", item.type)}>
-                {item.money.toFixed(2)}
-              </div>
-            </div>
-          );
-        })}
-      </div>
+            );
+          })}
+        </div>
+      )}
     </div>
   );
 };
