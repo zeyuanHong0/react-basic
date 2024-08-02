d02ec912c81bcbb7aac20112196fa6802345a6f1
Alexander Smith <hongzeyuan0@gmail.com>
2024-08-02 15:48:26 +0800

    useReducer demo

--- src/useReducer/index.jsx
+++ src/useReducer/index.jsx
@@ -1,5 +1,30 @@
+import { useReducer } from "react";
+
 const UseReducerDemo = () => {
-  return <div>我是useReducer</div>;
+  const countReducer = (state, action) => {
+    switch (action.type) {
+      case "INCREMENT":
+        return state + 1;
+      case "DECREMENT":
+        return state - 1;
+      case "SET":
+        return action.payload;
+      default:
+        return state;
+    }
+  };
+  const [count, dispatch] = useReducer(countReducer, 0);
+
+  return (
+    <div>
+      <button onClick={() => dispatch({ type: "INCREMENT" })}>+</button>
+      <span>{count}</span>
+      <button onClick={() => dispatch({ type: "DECREMENT" })}>-</button>
+      <button onClick={() => dispatch({ type: "SET", payload: 10 })}>
+        set
+      </button>
+    </div>
+  );
 };
 
 export default UseReducerDemo;
