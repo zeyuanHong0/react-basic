c48eca085c7629c1740149c63e21fa2ffb64fb1f
Alexander Smith <hongzeyuan0@gmail.com>
2024-08-05 20:48:42 +0800

    React.memo--props比较机制,对象比较地址值

--- src/memo/index.jsx
+++ src/memo/index.jsx
@@ -1,8 +1,13 @@
-import { memo, useState } from "react";
+import { memo, useMemo, useState } from "react";
 
-const Child = ({ name }) => {
+const Child = ({ name, list }) => {
   console.log("子组件渲染了");
-  return <div>{name}</div>;
+  return (
+    <div>
+      <div>{name}</div>
+      <div>{list}</div>
+    </div>
+  );
 };
 
 const MemoChile = memo(Child);
@@ -10,9 +15,13 @@ const MemoChile = memo(Child);
 const Father = () => {
   const [count, setCount] = useState(0);
   const [name, setName] = useState("张三");
+  // const list = [1, 2, 3];
+  const list = useMemo(() => {
+    return [1, 2, 3];
+  }, []);
   return (
     <div>
-      <MemoChile name={name} />
+      <MemoChile name={name} list={list} />
       <button onClick={() => setCount(count + 1)}>{count}</button>
       <button onClick={() => setName("李四")}>改变子组件名称</button>
     </div>
