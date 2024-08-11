b21e9075fa4b7a097990a07b3b773feeff2a70fe
Alexander Smith <hongzeyuan0@gmail.com>
2024-08-11 21:34:05 +0800

    zustand---异步支持

--- src/zuStand/index.jsx
+++ src/zuStand/index.jsx
@@ -1,12 +1,22 @@
 import useStore from "@/zuStandStore";
+import { useEffect } from "react";
 
 const Demo = () => {
-  const { count, increment, decrement } = useStore();
+  const { count, increment, decrement, getChannelList, channelList } =
+    useStore();
+  useEffect(() => {
+    getChannelList();
+  }, []);
   return (
     <div>
       <div>{count}</div>
       <button onClick={increment}>+1</button>
       <button onClick={decrement}>-1</button>
+      <ul>
+        {channelList.map((item) => (
+          <li key={item.id}>{item.name}</li>
+        ))}
+      </ul>
     </div>
   );
 };
--- src/zuStandStore/index.js
+++ src/zuStandStore/index.js
@@ -3,6 +3,7 @@ import { create } from "zustand";
 const useStore = create((set) => {
   return {
     count: 0,
+    channelList: [],
     increment() {
       set((state) => {
         return { count: state.count + 1 };
@@ -12,6 +13,13 @@ const useStore = create((set) => {
       set((state) => {
         return { count: state.count - 1 };
       });
+    },
+    async getChannelList() {
+      const res = await fetch("http://geek.itheima.net/v1_0/channels", {
+        method: "GET",
+      });
+      const jasonRes = await res.json();
+      set({ channelList: jasonRes.data.channels });
     },
   };
 });
